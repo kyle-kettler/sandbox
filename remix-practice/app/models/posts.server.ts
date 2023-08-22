dfa58fd0afc195ce1b27c115a80732c41091aea9
Kyle Kettler <hello@kylekettler.com>
2023-08-22 14:48:11 -0500

    remix app setup, posts route created, getPosts, loading posts and redering them as a list

---
+++ remix-practice/app/models/posts.server.ts
@@ -0,0 +1,14 @@
+export async function getPosts() {
+  const posts = [
+    {
+      slug: "my-fist-post",
+      title: "My First Post",
+    },
+    {
+      slug: "made-with-remix",
+      title: "This is made with Remix",
+    },
+  ];
+
+  return posts;
+}
